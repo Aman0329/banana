dc79cd36b6e2880801081f0a90818d9fbb06dd54
aman <samanjot94@gmail.com>
2022-07-13 03:03:44 +0530

    javascript and html done

--- script.js
+++ script.js
@@ -4,10 +4,26 @@ var output_trans=document.querySelector("#outputtrans");
 
 //taking input
 var input=input_trans.value;
+ 
+var serverurl="https://api.funtranslations.com/translate/minion.json"
 
+function getTranslated(text){
+    return serverurl + "?" +"text=" +text
+}
+
+function errorHandler(error){
+    console.log(error);
+}
 //button events
 button.addEventListener("click",function(){
+    fetch(getTranslated(input))
+    .then(response=>response.json())
+    .then(json=>{
+        var translatedtext=json.contents.translated;
+        console.log(translatedtext);
+        output_trans.innerHTML=translatedtext;
+    })
+    .catch(errorHandler)
     
-    
-})
+});
 
