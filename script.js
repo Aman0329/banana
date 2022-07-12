1fb8cfd5dd626de9a28597099aac91aba09c1969
aman <samanjot94@gmail.com>
2022-07-13 01:10:56 +0530

    taking input and translate button working

---
+++ script.js
@@ -0,0 +1,13 @@
+var button=document.querySelector("#btn");
+var input_trans=document.querySelector("#inputtrans");
+var output_trans=document.querySelector("#outputtrans");
+
+//taking input
+var input=input_trans.value;
+
+//button events
+button.addEventListener("click",function(){
+    
+    
+})
+
